a68f9f914064c70b76bf7e4bde7f8e9a09350215
naknamu <jodeldelvalle@gmail.com>
2022-12-09 13:56:31 +0800

    Added framer motion library to cart panel

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import Header from "./components/header";
 import Shop from "./components/shop";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
+import ShoppingCart from "./components/shopping-cart";
+import { useState } from "react";
 
 function App() {
+
+  const [showCart, setShowCart] = useState(false);
+
+  const handleCartClick = () => {
+      setShowCart(true);
+  }
+
   return (
     <>
       <BrowserRouter>
-        <Header />
+        <Header handleCartClick={handleCartClick}/>
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/shop" element={<Shop />} />
+          <Route path="/shopping-cart" element={<ShoppingCart />} />
         </Routes>
-      </BrowserRouter>
       <Footer />
+      <ShoppingCart showCart={showCart} setShowCart={setShowCart}/>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,25 @@
 import { FaShoppingCart } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
+
+    const {handleCartClick} = props;
+
     return ( 
+        <>
         <header className="pinaka">
             <div className="mataas">
-                <h1>MotorStore</h1>
+                <Link to="/"><h1>MotorStore</h1></Link>
                 <nav>
                     <Link to="/">Home</Link>
                     <Link to="/shop">Shop</Link>
-                    <div className='simbolo'><FaShoppingCart/></div>
+                    <div className='simbolo' onClick={handleCartClick}>
+                        <FaShoppingCart/>
+                    </div>
                 </nav>
             </div>
         </header>
+        </>
      );
 }
  
