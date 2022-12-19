5a1c4224d6ed1fc55d145689c228bee282393ba1
naknamu <jodeldelvalle@gmail.com>
2022-12-19 15:23:24 +0800

    Added loading spinner animation

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,38 @@
 import forza from "../videos/forza.mp4";
+import { FidgetSpinner } from  'react-loader-spinner';
+import { useState } from "react";
 
 const Home = () => {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleVideoEnd = () => {
+    setIsLoading(false);
+    console.log('load end!');
+  }
+
+  const handleVideoStart = () => {
+    setIsLoading(true);
+    console.log('load start!');
+  }
+
   return (
     <main>
+      {isLoading && 
+        <div className="loader">
+          <FidgetSpinner
+          visible={true}
+          height="500"
+          width="500"
+          ariaLabel="dna-loading"
+          wrapperStyle={{}}
+          wrapperClass="dna-wrapper"
+          ballColors={['#ff0000', '#FFFF00', '#0000ff']}
+          backgroundColor="#de182e"
+          />
+        </div>}
       <div className="home video">
-        <video src={forza} autoPlay muted loop></video>
+        <video src={forza} autoPlay muted loop onLoadStart={() => handleVideoStart()} onLoadedData={() => handleVideoEnd()}></video>
       </div>
     </main>
   );
